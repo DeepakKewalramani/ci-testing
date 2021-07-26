f588a302fb63ac4fc4e3b174a8a3b4967268380d
deepakkewalramani <deepakkewalramani08@gmail.com>
2021-07-26 11:53:29 +0530

    LandingPage

--- application/views/landing.php
+++ application/views/landing.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@200&display=swap" rel="stylesheet">
+    <script src="/Codeigniter/img/load.js"></script>
     <link rel="stylesheet" href="/Codeigniter/img/landstyle.css">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -15,6 +16,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <title>Loading Page</title>
 </head>
 
@@ -32,12 +35,12 @@
                     </div>
                     <div class="nav">
                         <div class="nav-item"><a href="#" class="nav-link nav-active">Home</a></div>
-                        <div class="nav-item"><a href="#" class="nav-link">Services Us</a></div>
-                        <div class="nav-item"><a href="#" class="nav-link">Why Us</a></div>
-                        <div class="nav-item"><a href="#" class="nav-link">Our Goals</a></div>
-                        <div class="nav-item"><a href="#" class="nav-link">Our Portfoilio</a></div>
-                        <div class="nav-item"><a href="#" class="nav-link">Our Client</a></div>
-                        <div class="nav-item"><a href="#" class="nav-link btn">Contact Us</a></div>
+                        <div class="nav-item"><a href="#service-us" class="nav-link">Services Us</a></div>
+                        <div class="nav-item"><a href="#why-us" class="nav-link">Why Us</a></div>
+                        <div class="nav-item"><a href="#goals" class="nav-link">Our Goals</a></div>
+                        <div class="nav-item"><a href="#portfolio" class="nav-link">Our Portfoilio</a></div>
+                        <div class="nav-item"><a href="#client" class="nav-link">Our Client</a></div>
+                        <div class="nav-item"><a href="#contact-us" class="nav-link btn">Contact Us</a></div>
                     </div>
                 </nav>
             </div>
@@ -58,34 +61,186 @@
             </div>
         </div>
         <section>
-        <div class="p-2 mt-5 text-center about">
-            <h1 class="heading">We provide various kind of service for you</h1>
-            <p class="para">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem </p>
-            <div class="about-us">
-                <div class="one">
-                <img src="/Codeigniter/img/smartphone.svg" alt="smartphone" width="150px">
-                <h1>unique app ui</h1>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+            <div class="p-2 mt-5 about" id="service-us">
+                <h1 class="heading">We provide various kind of service for you</h1>
+                <p class="para">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem </p>
+                <div class="about-us">
+                    <div class="one">
+                        <img src="/Codeigniter/img/smartphone.svg" alt="smartphone" width="150px">
+                        <h1>unique app ui</h1>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+                    </div>
+                    <div class="one">
+                        <img src="/Codeigniter/img/layout.svg" alt="smartphone" width="150px">
+                        <h1>exclient dashboard</h1>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+                    </div>
+                    <div class="one">
+                        <img src="/Codeigniter/img/smartphone.svg" alt="smartphone" width="150px">
+                        <h1>by construction</h1>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+                    </div>
                 </div>
-                <div class="one">
-                <img src="/Codeigniter/img/layout.svg" alt="smartphone"  width="150px">
-                <h1>exclient dashboard</h1>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page</p> 
+                <h1 class="heading" id="why-us">why us</h1>
+                <p class="para">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem </p>
+                <div class="about-section">
+                    <div class="left">
+                        <div class="box"></div>
+                        <div class="radius"></div>
+                        <img src="/Codeigniter/img/iPhoneLight.png" width="200px">
+                    </div>
+                    <div class="right">
+                        <h1>Built The App That Everyone Love.</h1>
+                        <P>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters</P>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using</p>
+                        <button class="btn"> get more <img src="/Codeigniter/img/arrow_right_black.svg" width="50px"></button>
+                    </div>
                 </div>
-                <div class="one">
-                <img src="/Codeigniter/img/smartphone.svg" alt="smartphone"  width="150px">
-                <h1>by construction</h1>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page</p>
+                <div class="about-section-1" id="goals">
+                    <h1 class="heading">we completed 1200+ projects yearly successfully & counting</h1>
+                    <p class="para">It is a long established fact that a reader will be distracted by the readable </p>
+                    <p class="para">It is a long established fact </p>
+                    <div class="about-us-1">
+                        <div class="sections">
+                            <img src="/Codeigniter/img/check_circle_outline.svg" alt="" width="60px">
+                            <div class="sec-text">
+                                <h1>100+</h1>
+                                <p>projects complete</p>
+                            </div>
+                        </div>
+                        <div class="sections">
+                            <img src="/Codeigniter/img/assignment_turned_in-24px.svg" alt="" width="60px">
+                            <div class="sec-text">
+                                <h1>100+</h1>
+                                <p>active projects</p>
+                            </div>
+                        </div>
+                        <div class="sections">
+                            <img src="/Codeigniter/img/check_circle_outline.svg" alt="" width="60px">
+                            <div class="sec-text">
+                                <h1>90+</h1>
+                                <p>client satisfied</p>
+                            </div>
+                        </div>
+                        <div class="sections">
+                            <img src="/Codeigniter/img/check_circle_outline.svg" alt="" width="60px">
+                            <div class="sec-text">
+                                <h1>56+</h1>
+                                <p>country available</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="about-section-1">
+                        <h1 class="heading" id="portfolio">our portfolio</h1>
+                        <p class="para">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem</p>
+                        <div class="about-us-2">
+                            <div class="portfolio-img1"><img src="/Codeigniter/img/iPhoneLight.png" width="260px"></div>
+                            <div class="portfolio-img2"><img src="/Codeigniter/img/iPhoneLight-2.png" width="260px"></div>
+                            <div class="portfolio-img3"><img src="/Codeigniter/img/iPhoneLight-1.png" width="260px"></div>
+                            <div class="portfolio-img4"><img src="/Codeigniter/img/iPhoneLight-1.png" width="260px"></div>
+                            <div class="port-box"></div>
+                        </div>
+                    </div>
+
+                    <div class="feedback-section" id="client">
+                        <div class="feedback">
+                            <h1>our clients feedback</h1>
+                            <P>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is</P>
+                        </div>
+                        <div>
+                            <a href="#"> Read More <img src="/Codeigniter/img/arrow_right_black.svg" width="50px"></a>
+                        </div>
+                    </div>
+
+                    <div class="feedback-review">
+                        <div class="feedback-img">
+                            <img src="/Codeigniter/img/photography-of-a-guy.png" alt="pic" width="260px">
+                        </div>
+                        <div class="feedback-text">
+                            <div class="rating">
+                                <img src="/Codeigniter/img/grade-24px.svg" alt="rating" width="40px">
+                                <img src="/Codeigniter/img/grade-24px.svg" alt="rating" width="40px">
+                                <img src="/Codeigniter/img/grade-24px.svg" alt="rating" width="40px">
+                                <img src="/Codeigniter/img/grade-24px.svg" alt="rating" width="40px">
+                                <img src="/Codeigniter/img/grade-24px.svg" alt="rating" width="40px">
+                            </div>
+                            <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo</p>
+                            <h1>riad isalm</h1>
+                            <h2>product manager.<span> @pixbay</span></h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="contact-form" id="contact-us">
+                    <div class="flex">
+                        <div class="contact">
+                            <h1>contact us!
+                            </h1>
+                            <p>There are many variations of passages of Lorem Ipsum but the majority have suffered alteration.</p>
+                        </div>
+                        <div class="form">
+                            <input type="text" placeholder="Email Here">
+                            <button type="submit">Send</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer">
+                    <div class="foot-sec">
+                        <div class="section-1">
+                            <div class="icon-1"></div>
+                            <div class="icon-2"></div>
+                            <h1>pixbey</h1>
+                            <div class="social-icons">
+                            <a href=""><i class="bi bi-facebook media-icons"></i></a>
+                            <a href=""><i class="bi bi-twitter media-icons"></i></a>
+                            <a href=""><i class="bi bi-linkedin media-icons"></i></a>
+                            <a href=""><i class="bi bi-instagram media-icons"></i></a>
+                            </div>
+                        </div>
+                        <div class="section-2">
+                            <h1>links</h1>
+                            <div class="links">
+                                <a href="#">Home</a>
+                                <a href="#">Pricing</a>
+                                <a href="#">Download</a>
+                                <a href="#">About</a>
+                                <a href="#">service</a>
+                            </div>
+                        </div>
+                        <div class="section-3">
+                            <h1>Support</h1>
+                            <div class="links">
+                                <a href="#">FAQ</a>
+                                <a href="#">How it</a>
+                                <a href="#">Features</a>
+                                <a href="#">Contact</a>
+                                <a href="#">Reporting</a>
+                            </div>
+                        </div>
+                        <div class="section-4">
+                            <h1>Contact Us</h1>
+                            <div class="links">
+                                <a href="tel:+880 12345678">+880 12345678</a>
+                                <a href="mailto:kookyinfomedia@gmail.com">youremail@gmail.com</a>
+                                <a href="#">Rangpur City</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-about">
+                    <div><p>Copyright & Design by @md riad islam</p></div>
+                    <div style="display: flex; padding:10px;">
+                        <a href="">Terms of use</a>
+                        <a href="">Privacy Policy</a>
+                </div>
+                    </div>
                 </div>
             </div>
-            <h1 class="heading">why us</h1>
-            <p class="para">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem </p>
-            <div class="radius">
-            <div class="img"><img src="/Codeigniter/img/iPhoneLight.png" width="320px"></div>
-            </div>
-            
-        </div>
-        </section>
+    </div>
+
+
+    </div>
+    </div>
+
+    </section>
     </div>
 </body>
 
---
+++ img/load.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function() {
+    var element = window;
+    console.log(element);
+   console.log(window.pageYOffset + 'px');
+  });
